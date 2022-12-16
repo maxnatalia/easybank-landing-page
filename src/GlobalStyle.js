6142e74bbd08e871c7b8749eb4a341247fbb5045
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-16 10:09:58 +0100

    added GlobalStyle

---
+++ src/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
+
+export const GlobalStyle = createGlobalStyle`
+    ${normalize}
+
+    html {
+    box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+    box-sizing: inherit;
+    }
+
+    body {
+        font-family: 'Public Sans', sans-serif;
+        font-size: 18px;
+    }
+`;
