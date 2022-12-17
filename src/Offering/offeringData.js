a65271749a5483d084dd96a73e6787550671c69e
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-17 11:28:09 +0100

    created file offeringData.js

---
+++ src/Offering/offeringData.js
@@ -0,0 +1,31 @@
+import api from "./icon-api.svg";
+import online from "./icon-online.svg";
+import budgeting from "./icon-budgeting.svg";
+import onboarding from "./icon-onboarding.svg";
+
+export const offeringData = [
+    {
+        id: 1,
+        icon: online,
+        title: "Online Banking",
+        description: "Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.",
+    },
+    {
+        id: 2,
+        icon: budgeting,
+        title: "Simple Budgeting",
+        description: "See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.",
+    },
+    {
+        id: 3,
+        icon: onboarding,
+        title: "Fast Onboarding",
+        description: "We don’t do branches. Open your account in minutes online and start taking control of your finances right away.",
+    },
+    {
+        id: 4,
+        icon: api,
+        title: "Open API",
+        description: "Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.",
+    },
+];
