684504a81d455ebe97fe7c0ea50f75276f5529cf
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-17 11:28:48 +0100

    added simples styles for Offering component

---
+++ src/Offering/styled.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const OfferingContent = styled.div`
+    width: 100%;
+    background-color: ${({ theme }) => theme.colors.lightGrayishBlue};
+    z-index: 50;
+
+`;
+export const Title = styled.h2`
+`;
+
+export const Info = styled.p`
+`;
+
+export const Container = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+`;
+
+export const Offert = styled.div`
+`;
+export const Icon = styled.img`
+`;
+export const Subtitle = styled.h3`
+`;
+export const Content = styled.p`
+`;
