09349bfaa60e07de30add704c3ae6858973d644a
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-18 09:32:50 +0100

    created articlesData.js

---
+++ src/Articles/articlesData.js
@@ -0,0 +1,35 @@
+import restaurant from "./image-restaurant.jpg";
+import currency from "./image-currency.jpg";
+import plane from "./image-plane.jpg";
+import confetti from "./image-confetti.jpg";
+
+export const articlesData = [
+    {
+        id: 1,
+        image: restaurant,
+        person: "By Claire Robinson",
+        title: "Receive money in any currency with no fees",
+        content: "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …",
+    },
+    {
+        id: 2,
+        image: currency,
+        person: "By Wilson Hutton",
+        title: "Treat yourself without worrying about money",
+        content: "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …",
+    },
+    {
+        id: 3,
+        image: plane,
+        person: "By Wilson Hutton",
+        title: "Take your Easybank card wherever you go",
+        content: "We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …",
+    },
+    {
+        id: 4,
+        image: confetti,
+        person: "By Claire Robinson",
+        title: "Our invite-only Beta accounts are now live!",
+        content: "After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ...",
+    }
+];
