2c741b77777f04a1e0982b0b4bf3b6ea962b2264
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-18 09:33:15 +0100

    destructurization Offering component

--- src/Offering/index.js
+++ src/Offering/index.js
@@ -5,14 +5,14 @@ const Offering = () => {
     return (
         <OfferingContent>
             <Title> Why choose Easybank?</Title>
-            <Info> We leverage Open Banking to turn your bank account into your financial hub. Control
-                your finances like never before.</Info>
+            <Info> We leverage Open Banking to turn your bank account into your financial hub.
+                Control your finances like never before.</Info>
             <Container>
-                {offeringData.map((item) => (
-                    <Offert key={item.id}>
-                        <Icon src={item.icon} alt={item.title} />
-                        <Subtitle>{item.title}</Subtitle>
-                        <Content>{item.description}</Content>
+                {offeringData.map(({ id, title, icon, description }) => (
+                    <Offert key={id}>
+                        <Icon src={icon} alt={title} />
+                        <Subtitle>{title}</Subtitle>
+                        <Content>{description}</Content>
                     </Offert>
                 ))}
             </Container>
