7a2bc1b947a44852ecd272197863dd57c3207613
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-19 07:20:30 +0100

    added new component Articles

---
+++ src/Articles/index.js
@@ -0,0 +1,21 @@
+import { articlesData } from "./articlesData";
+import { Title, Article, Image, Author, Subtitle, Content, Container, ArticleContainer } from "./styled";
+
+const Articles = () => {
+    return (
+        <ArticleContainer>
+            <Title>Latest Articles</Title>
+            <Container>
+                {articlesData.map(({ id, title, image, person, content }) =>
+                    <Article key={id}>
+                        <Image src={image} alt={title} />
+                        <Author>{person}</Author>
+                        <Subtitle>{title}</Subtitle>
+                        <Content>{content}</Content>
+                    </Article>
+                )}
+            </Container>
+        </ArticleContainer>);
+};
+
+export default Articles;
