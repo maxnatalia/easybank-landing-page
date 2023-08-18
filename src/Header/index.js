ffee7cd6833feacbc20f6dde5f0bf8b1b318f3d5
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-18 10:33:38 +0200

    updated - added Wrapper to Header component

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,23 +1,26 @@
-import { BoxContent, BoxImage, Content, Image, PhoneImage, StyledHeader, Title } from "./styled";
-
+import { BoxContent, BoxImage, Content, PhoneImage, StyledHeader, Title, Wrapper } from "./styled";
 import { Button } from "../Button/Button";
 
 const Header = () => {
     return (
         <StyledHeader>
-            <BoxContent>
-                <Title>
-                    Next generation digital banking
-                </Title>
-                <Content>
-                    Take your financial life online. Your Easybank account will be a one-stop-shop
-                    for spending, saving, budgeting, investing, and much more.
-                </Content>
-                <Button>
-                    Request Invite
-                </Button>
-            </BoxContent>
-            <BoxImage><PhoneImage /></BoxImage>
+            <Wrapper>
+                <BoxContent>
+                    <Title>
+                        Next generation digital banking
+                    </Title>
+                    <Content>
+                        Take your financial life online. Your Easybank account will be a one-stop-shop
+                        for spending, saving, budgeting, investing, and much more.
+                    </Content>
+                    <Button>
+                        Request Invite
+                    </Button>
+                </BoxContent>
+                <BoxImage>
+                    <PhoneImage />
+                </BoxImage>
+            </Wrapper>
         </StyledHeader>
     )
 };
