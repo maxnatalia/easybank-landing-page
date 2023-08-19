a15d07222651309da09beb2ffdd91912ab7a043e
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-19 07:48:33 +0200

    added Overlay and show props

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,30 +1,39 @@
 import { useState } from "react";
 import { Button } from "../Button/Button";
-import { StyledNav, StyledLogo, LinksContainer, StyledNavLink, MobileClose, MobileOpen, MobileNav } from "./styled";
+import { ReactComponent as MobileClose } from "./icon-close.svg";
+import { ReactComponent as MobileOpen } from "./icon-hamburger.svg";
+import { StyledNav, StyledLogo, LinksContainer, StyledNavLink, MobileNav, Wrapper, ButtonWrapper, Overlay } from "./styled";
 
 const Navigation = () => {
-
     const [show, setShow] = useState(false);
 
     const handleClick = () => {
         setShow(!show)
     };
-    return (
-        <StyledNav>
-            <StyledLogo />
-            <MobileNav onClick={handleClick}>
-                {show ? <MobileOpen /> : <MobileClose />}
 
-            </MobileNav>
-            <LinksContainer show={show}>
-                <StyledNavLink>Home</StyledNavLink>
-                <StyledNavLink>About</StyledNavLink>
-                <StyledNavLink>Contact</StyledNavLink>
-                <StyledNavLink>Blog</StyledNavLink>
-                <StyledNavLink>Careers</StyledNavLink>
-            </LinksContainer>
-            <Button navigation>Request Invite</Button>
-        </StyledNav>);
+    return (
+        <>
+            {!show && <Overlay />}
+            <StyledNav>
+                <Wrapper>
+                    <StyledLogo />
+                    <MobileNav onClick={handleClick}>
+                        {show ? <MobileOpen /> : <MobileClose />}
+                    </MobileNav>
+                    <LinksContainer show={show}>
+                        <StyledNavLink href="/">Home</StyledNavLink>
+                        <StyledNavLink href="/">About</StyledNavLink>
+                        <StyledNavLink href="/">Contact</StyledNavLink>
+                        <StyledNavLink href="/">Blog</StyledNavLink>
+                        <StyledNavLink href="/">Careers</StyledNavLink>
+                    </LinksContainer>
+                    <ButtonWrapper>
+                        <Button navigation>Request Invite</Button>
+                    </ButtonWrapper>
+                </Wrapper>
+            </StyledNav>
+        </>
+    );
 };
 
 export default Navigation;
