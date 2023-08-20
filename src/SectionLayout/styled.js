1bf5b3799f1bfe493acf10c36fe05a7bf2d0a7df
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-20 07:23:20 +0200

    improved Wrapper and added other divices styles

--- src/SectionLayout/styled.js
+++ src/SectionLayout/styled.js
@@ -1,36 +1,48 @@
 import styled from "styled-components";
 
 export const Section = styled.section`
-    width: 100%;
-    margin: 60px auto;
-    padding: 40px 100px;
+    width: 100vw;
     background-color: ${({ theme, backgroundColor }) =>
         backgroundColor ? theme.colors.lightGrayishBlue : 'transparent'};
 
     @media (max-width: 767px) {
-        margin: 40px auto;
-        padding: 20px;
+        /* margin: 40px auto; */
+
     }
 `;
 
 export const Wrapper = styled.div`
-    max-width: 1400px;
+    min-height: 670px;
+    max-width: 1270px;
     margin: 0 auto;
+    padding-left: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 20px;
+
+    @media (max-width: 1199px) {
+        padding: 40px 20px;
+        text-align: center;
+    }
 `;
 
 export const Title = styled.h2`
+    margin: 0;
     color: ${({ theme }) => theme.colors.darkBlue};
     font-weight: 300;
     font-size: 34px;
 `;
 
 export const Info = styled.p`
+    margin: 0 0 40px 0;
     color: ${({ theme }) => theme.colors.grayishBlue};
     font-weight: 300;
     line-height: 1.7;
     max-width: 650px;
 
-    @media (max-width: 767px) {
+    @media (max-width: 1199px) {
         font-size: 16px;
+        margin: 0 auto;
     }
 `;
