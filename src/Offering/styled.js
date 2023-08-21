9ab01b627b44ace8b097ce54c134e62f306ce604
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-21 07:36:59 +0200

    added styles for different devices

--- src/Offering/styled.js
+++ src/Offering/styled.js
@@ -1,57 +1,39 @@
 import styled from "styled-components";
 
-export const OfferingContent = styled.div`
-    width: 100%;
-    background-color: ${({ theme }) => theme.colors.lightGrayishBlue};
-    z-index: 50;
-    padding: 110px 130px;
-    margin-top: 80px;
-
-    @media (max-width: 767px) {
-        padding: 20px;
-    }
-
-`;
-export const Title = styled.h2`
-    color: ${({ theme }) => theme.colors.darkBlue};
-    font-weight: 300;
-
-    @media (max-width: 767px) {
-        text-align: center;
-    }
-`;
-
-export const Info = styled.p`
-    color: ${({ theme }) => theme.colors.grayishBlue};
-    font-weight: 300;
-    max-width: 650px;
-
-    @media (max-width: 767px) {
-        text-align: center;
-    }
-`;
-
 export const Container = styled.div`
+    width: 100%;
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-template-columns: repeat(4, 280px);
+    grid-template-rows: 1fr;
+    justify-content: space-between;
+    align-items: center;
+    grid-gap: 20px;
+
+    @media (max-width: 1199px) {
+        grid-template-columns: repeat(2, 280px);
+        grid-template-rows: repeat(2, 1fr);
+        justify-content: center;
+    }
 
     @media (max-width: 767px) {
-        text-align: center;
+        grid-template-columns: 280px;
+        grid-template-rows: repeat(4, 1fr);
+        justify-content: center;
     }
-    
 `;
 
 export const Offert = styled.div`
-    padding: 30px 20px 30px 0;
+    /* max-width: 280px; */
 `;
-export const Icon = styled.img`
-`;
-export const Subtitle = styled.h3`
+
+export const Name = styled.h3`
     color: ${({ theme }) => theme.colors.darkBlue};
     font-weight: 300;
 `;
+
 export const Content = styled.p`
     color: ${({ theme }) => theme.colors.grayishBlue};
     font-weight: 300;
-    line-height: 1.5;
+    font-size: 16px;
+    line-height: 1.6;
 `;
