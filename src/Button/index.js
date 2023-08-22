916df8391262aed46d84eb6f958ee6790a2f6845
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-22 11:18:21 +0200

    created reusable component Button

---
+++ src/Button/index.js
@@ -0,0 +1,11 @@
+import { StyledButton } from "./styled";
+
+const Button = ({ navigation }) => {
+    return (
+        <StyledButton navigation={navigation}>
+            Request Invite
+        </StyledButton>
+    )
+};
+
+export default Button;
