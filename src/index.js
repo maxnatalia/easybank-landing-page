c61c7995ca09b9fb6beb6339c5cc01bed3e5887b
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-23 11:39:56 +0200

    refactoring for index - removed unnecessary lines

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from 'styled-components';
 import { theme } from "./theme";
+import { GlobalStyle } from "./GlobalStyle";
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,8 +14,3 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
