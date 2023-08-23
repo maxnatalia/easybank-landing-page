18c0929f4194c69852b40cf9f7e3013f911ed8db
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-23 11:49:44 +0200

    improved styles - removed unnecessary container

--- src/Offering/index.js
+++ src/Offering/index.js
@@ -1,6 +1,6 @@
 import SectionLayout from "../SectionLayout";
 import { offeringData } from "./offeringData";
-import { Container, Offert, Name, Content } from "./styled";
+import { Name, Content } from "./styled";
 
 const Offering = () => {
     return (
@@ -9,15 +9,13 @@ const Offering = () => {
             backgroundColor
             info={"We leverage Open Banking to turn your bank account into your financial hub. Control your finances like never before."}
         >
-            <Container>
-                {offeringData.map(({ id, title, icon, description }) => (
-                    <Offert key={id}>
-                        <img src={icon} alt={title} />
-                        <Name>{title}</Name>
-                        <Content>{description}</Content>
-                    </Offert>
-                ))}
-            </Container>
+            {offeringData.map(({ id, title, icon, description }) => (
+                <div key={id}>
+                    <img src={icon} alt={title} />
+                    <Name>{title}</Name>
+                    <Content>{description}</Content>
+                </div>
+            ))}
         </SectionLayout>
     );
 };
--- src/Offering/styled.js
+++ src/Offering/styled.js
@@ -1,31 +1,5 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    width: 100%;
-    display: grid;
-    grid-template-columns: repeat(4, 280px);
-    grid-template-rows: 1fr;
-    justify-content: space-between;
-    align-items: center;
-    grid-gap: 20px;
-
-    @media (max-width: 1199px) {
-        grid-template-columns: repeat(2, 280px);
-        grid-template-rows: repeat(2, 1fr);
-        justify-content: center;
-    }
-
-    @media (max-width: 767px) {
-        grid-template-columns: 280px;
-        grid-template-rows: repeat(4, 1fr);
-        justify-content: center;
-    }
-`;
-
-export const Offert = styled.div`
-    /* max-width: 280px; */
-`;
-
 export const Name = styled.h3`
     color: ${({ theme }) => theme.colors.darkBlue};
     font-weight: 300;
