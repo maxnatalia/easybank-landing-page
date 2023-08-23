1a6be430d30568e51f74a85179c4733af7e75821
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-23 11:37:25 +0200

    implemented navLinks and click close menu

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,34 +1,47 @@
 import { useState } from "react";
-import { Button } from "../Button/Button";
+import Button from "../Button";
 import { ReactComponent as MobileClose } from "./icon-close.svg";
 import { ReactComponent as MobileOpen } from "./icon-hamburger.svg";
-import { StyledNav, StyledLogo, LinksContainer, StyledNavLink, MobileNav, Wrapper, ButtonWrapper, Overlay } from "./styled";
+import { navigationLinks } from "./data";
+import {
+    StyledNav,
+    StyledLogo,
+    LinksContainer,
+    StyledNavLink,
+    MobileNav,
+    Wrapper,
+    ButtonWrapper,
+    Overlay
+} from "./styled";
 
 const Navigation = () => {
     const [show, setShow] = useState(false);
 
     const handleClick = () => {
-        setShow(!show)
+        setShow(show => !show)
     };
 
     return (
         <>
-            {!show && <Overlay />}
+            {show && <Overlay />}
             <StyledNav>
                 <Wrapper>
                     <StyledLogo />
                     <MobileNav onClick={handleClick}>
-                        {show ? <MobileOpen /> : <MobileClose />}
+                        {show ? <MobileClose /> : <MobileOpen />}
                     </MobileNav>
                     <LinksContainer show={show}>
-                        <StyledNavLink href="/">Home</StyledNavLink>
-                        <StyledNavLink href="/">About</StyledNavLink>
-                        <StyledNavLink href="/">Contact</StyledNavLink>
-                        <StyledNavLink href="/">Blog</StyledNavLink>
-                        <StyledNavLink href="/">Careers</StyledNavLink>
+                        {navigationLinks.map(({ name, href }) => <StyledNavLink
+                            key={name}
+                            href={href}
+                            onClick={handleClick}
+                        >
+                            {name}
+                        </StyledNavLink>
+                        )}
                     </LinksContainer>
                     <ButtonWrapper>
-                        <Button navigation>Request Invite</Button>
+                        <Button navigation />
                     </ButtonWrapper>
                 </Wrapper>
             </StyledNav>
