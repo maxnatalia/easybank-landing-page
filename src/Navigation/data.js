9f1ea4406f0ad63451c246716c7dcc07e7aea078
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-23 11:36:57 +0200

    added data for Navigation links

---
+++ src/Navigation/data.js
@@ -0,0 +1,22 @@
+export const navigationLinks = [
+    {
+        name: "Home",
+        href: "/"
+    },
+    {
+        name: "About",
+        href: "/"
+    },
+    {
+        name: "Contact",
+        href: "/"
+    },
+    {
+        name: "Blog",
+        href: "/"
+    },
+    {
+        name: "Careers",
+        href: "/"
+    },
+]
