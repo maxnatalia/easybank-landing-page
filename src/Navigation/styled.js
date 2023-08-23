7e8b72697227719eddb255bfe41266097f810aed
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-23 11:38:32 +0200

    improved styles for mobile devices

--- src/Navigation/styled.js
+++ src/Navigation/styled.js
@@ -6,13 +6,13 @@ export const StyledNav = styled.nav`
     height: 60px;
     background-color: ${({ theme }) => theme.colors.white};
     position: sticky;
-    z-index: 100;
+    z-index: 600;
 `;
 
 export const Wrapper = styled.div`
     max-width: 1270px;
-    height: 100%;
     margin: 0 auto;
+    height: 100%;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -29,8 +29,8 @@ export const LinksContainer = styled.div`
     justify-content: space-between;
 
     @media (max-width: 767px) {
-        opacity: ${({ show }) => show ? "0" : "1"};
-        visibility: ${({ show }) => show ? "hidden" : "visible"};
+        opacity: ${({ show }) => show ? "1" : "0"};
+        visibility: ${({ show }) => show ? "visible" : "hidden"};
         flex-direction: column;
         position: absolute;
         top: 80px;
@@ -66,7 +66,7 @@ export const StyledNavLink = styled.a`
             left: 0;
             width: 100%;
             height: 6px;
-            background-image: linear-gradient(90deg, rgba(49, 211, 92, 1) 0%, rgba(43, 183, 218, 1) 100%);
+            background-image: ${({ theme }) => theme.gradient.main};
 
             @media (max-width: 767px) {
                 display: none;
@@ -75,7 +75,7 @@ export const StyledNavLink = styled.a`
     }
 
     @media (max-width: 767px) {
-        padding: 18px;
+        padding: 12px;
         letter-spacing: 1px;
         font-size: 22px;
         color: ${({ theme }) => theme.colors.darkBlue};
@@ -115,11 +115,10 @@ export const Overlay = styled.div`
         position: fixed;
         top: 0;
         left: 0;
-        right: 0;
-        bottom: 0;
         width: 100%;
         height: 100%;
-        z-index: -1;
-        background-image: linear-gradient(${({ theme }) => theme.colors.darkBlue}, ${({ theme }) => theme.colors.white});
+        z-index: 500;
+        background-image: linear-gradient(${({ theme }) => theme.colors.darkBlue}, ${({ theme }) => theme.colors.lightGrayishBlue});
+        opacity: .8;
     }
 `;
