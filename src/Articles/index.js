20d04993f378e7bff3b31c0df732d311c018d5ba
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-23 11:40:43 +0200

    implemented common SectionLayout

--- src/Articles/index.js
+++ src/Articles/index.js
@@ -1,21 +1,21 @@
 import { articlesData } from "./articlesData";
-import { Title, Article, Image, Author, Subtitle, Content, Container, ArticleContainer } from "./styled";
+import SectionLayout from "../SectionLayout";
+import { Article, Image, Author, Subtitle, Content, Box } from "./styled";
 
 const Articles = () => {
     return (
-        <ArticleContainer>
-            <Title>Latest Articles</Title>
-            <Container>
-                {articlesData.map(({ id, title, image, person, content }) =>
-                    <Article key={id}>
-                        <Image src={image} alt={title} />
+        <SectionLayout title={"Latest Articles"}>
+            {articlesData.map(({ id, title, image, person, content }) =>
+                <Article key={id}>
+                    <Image src={image} alt={title} />
+                    <Box>
                         <Author>{person}</Author>
-                        <Subtitle>{title}</Subtitle>
+                        <Subtitle as="a" href="/">{title}</Subtitle>
                         <Content>{content}</Content>
-                    </Article>
-                )}
-            </Container>
-        </ArticleContainer>
+                    </Box>
+                </Article>
+            )}
+        </SectionLayout>
     );
 };
 
